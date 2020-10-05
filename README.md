7b8103bb8e4f9c67f7920a77a9011fa25cfeefb3
Skelatorian <mylynhle21@gmail.com>
2020-10-04 23:19:01 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# README-generator
+
+
+# Professional README Generator
+
+## Description 
+
+This application will automatically generate a professional README for your convience!
+
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#Contributing)
+* [License](#license)
+* [Test](#Test)
+* [Questions](#questions) 
+
+
+## Installation
+
+npm i
+
+
+## Usage 
+
+inquirer=requirer('inquirer');
+
+
+## Contributing
+
+[https://github.com/Xandromus] (https://github.com/Xandromus)
+
+
+## License
+
+Professional README Generator is licensed under the
+
+![](https://img.shields.io/static/v1?label=License&message=MIT&color=green&&style=plastic)
+
+MIT License
+
+A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+Permissions | Limitations  |   Conditions
+------------ | ------------  | ------------
+Commercial use | Liability |   License and copyright notice
+Modification | Warranty    |
+Distribution |     |
+Private use |     |
+
+
+
+
+
+## Test
+
+node index.js //# press enter
+
+
+## Questions
+
+Feel free to reach out! 
+
+
+Github Profile: [https://github.com/skelatorian] 
+
+E-mail: [mylynhle21@gmail.com] (mylynhle21@gmail.com)
--- index.js
+++ index.js
@@ -13,5 +13,158 @@ const promptUser = dataReadme => {
 Generating a Professional README file in Node.js
 ================================================
 `);
+
+  return inquirer.prompt([
+    // Section README Title
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is project title? (Required): ',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter your Project Title! ');
+          return false;
+        }
+      }
+    },
+ // Section Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Bring a brief description about your Project (Required): ',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter a Description! ');
+            return false;
+          }
+        }
+    },
+// Section Instalation
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How to install (Required): ',
+        validate: installInput => {
+            if (installInput) {
+            return true;
+            } else {
+            console.log('Place the way to install!');
+            return false;
+            }
+        }
+    },
+// Section Usage
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to enter some information about the "Usage" section? ',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide some examples or code to show the Application Usage: ',
+        when: ({ confirmUsage }) => {
+            if (confirmUsage) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+    },
+    // Section Contributions Guidelines
+    {
+        type: 'confirm',
+        name: 'confirmContribution',
+        message: 'Would you like to enter some information about the "Contribution" section? ',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Mention and Aknowledge the Contributors: ',
+        when: ({ confirmContribution }) => {
+            if (confirmContribution) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+    },
+// Section Test
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Give a few examples of running code (Required): ',
+        validate: testInput => {
+            if (testInput) {
+            return true;
+            } else {
+            console.log('Please give some examples for testing coding!');
+            return false;
+            }
+        }
+    },
+// Section Badges - License
+    {
+        type: 'rawlist',
+        name: 'license',
+        message: 'What Technologies did you use in this project with? (Check all that apply)',
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'Mozilla Public License 2.0']
+    },
+//Questions
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required): ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your Githubname!');
+          return false;
+        }
+      }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please Enter your email (Required): ',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your email!');
+            return false;
+          }
+        }
+      },
+  ]);
 };
+
+
+
+promptUser()
+  .then( data => {
+    //console.log(data);
+    return generateREADME(data);
+  })
+   .then(fileReadme => {
+     return writeFile(fileReadme);
+   })
+  .then(writeFileResponse => {
+    console.log(`
+================================================
+Finilizing. Wait while the App create the file
+================================================
+`);
+    console.log(writeFileResponse);
+   })
+  .catch(err => {
+    console.log(err);
+  });
 
